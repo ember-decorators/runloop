5afefd9e2cface49e2e35b1038e8becaa3069add
snewcomer24 <snewcomer24@gmail.com>
2017-12-05 21:41:16 -0800

    first shot at run.next

---
+++ tests/unit/runloop-test.js
@@ -0,0 +1,22 @@
+import { next } from '@ember-decorators/runloop';
+import { module, test } from 'qunit';
+
+module('computed properties');
+
+test('"run.next" works with es6 class', function(assert) {
+  let done = assert.async();
+  assert.expect(1);
+
+  class Foo {
+    @next
+    nextMe() {
+      assert.ok(true);
+    }
+  }
+
+  let obj = new Foo();
+  setTimeout(() => {
+    obj.nextMe();
+    done();
+  }, 20);
+});
